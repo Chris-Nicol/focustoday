8eda3cabeb0c7efbbdb14a859b0f4cc9136b5176
Chris Nicol <chrisn@iqmetrix.com>
2017-10-12 22:10:13 -0700

    first commit

---
+++ focustoday.webapp/focustoday.webapp/Controllers/api_controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace focustoday.webapp.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [Route("hello")]
+        [HttpGet]
+        // GET: /<controller>/
+        public IActionResult Hello()
+        {
+            var msg = new { Message = "Hello World" };
+            return this.Ok(msg);
+        }
+    }
+}
